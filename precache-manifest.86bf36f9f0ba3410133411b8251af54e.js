8af03416d7abdd4d41e39cd690f02516800f72cf
Ridalord <ridhwan.alabi@gmail.com>
2023-01-31 21:57:01 +0100

    Updates

---
+++ precache-manifest.86bf36f9f0ba3410133411b8251af54e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c29d826cf38414a3d6ec",
+    "url": "/react-portfolio/static/css/main.ec1027ff.chunk.css"
+  },
+  {
+    "revision": "c29d826cf38414a3d6ec",
+    "url": "/react-portfolio/static/js/main.c29d826c.chunk.js"
+  },
+  {
+    "revision": "53057543170e459e1941",
+    "url": "/react-portfolio/static/css/1.8934b938.chunk.css"
+  },
+  {
+    "revision": "53057543170e459e1941",
+    "url": "/react-portfolio/static/js/1.53057543.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "4d5e4f2a97e627ec7d53d0f747ff4f18",
+    "url": "/react-portfolio/static/media/WhatsApp Image 2022-10-30 at 21.04.16.4d5e4f2a.jpg"
+  },
+  {
+    "revision": "ea2de5fce406bb420e9919d3f9763974",
+    "url": "/react-portfolio/static/media/IMG_03867.ea2de5fc.jpg"
+  },
+  {
+    "revision": "cbe0d40ac7e042de56cd57001bdd4d79",
+    "url": "/react-portfolio/static/media/profile-img1.cbe0d40a.jpeg"
+  },
+  {
+    "revision": "21627f0e02cc7b9c71564da3ef5e3265",
+    "url": "/react-portfolio/index.html"
+  }
+];
