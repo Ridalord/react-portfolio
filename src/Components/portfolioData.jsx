966ea9294f758779a1d6aeb7e655008e7e0dc01a
Ridwan <ridhwan.alabi@gmail.com>
2024-01-02 18:50:29 +0100

    Updating projects

--- src/Components/portfolioData.jsx
+++ src/Components/portfolioData.jsx
@@ -1,5 +1,19 @@
 const portfolioData = [
     {
+        id: 70,
+        name: "Get Linked",
+        imgUrl: "https://i.ibb.co/P1KNtPG/getlinked.jpg",
+        link: "https://ridalord.github.io/get-linked/",
+        frameworks: ["https://i.ibb.co/ZGSVqWV/html.png", "https://i.ibb.co/NFnjCkb/css.png", "https://i.ibb.co/3d3ft1Z/react.png"]
+    },
+    {
+        id: 0,
+        name: "Airomis Services Limited",
+        imgUrl: "https://i.ibb.co/QY2Y1vn/airomisservices.jpg",
+        link: "www.airomis-services.com",
+        frameworks: ["https://i.ibb.co/ZGSVqWV/html.png", "https://i.ibb.co/NFnjCkb/css.png", "https://i.ibb.co/bvQh49M/javascript.png", "https://i.ibb.co/Zg5CmLY/tailwind-css-256x154.png"]
+    },
+    {
         id: 1,
         name: "Abosede Aina Charity Foundation",
         imgUrl: "https://i.ibb.co/7jSGnwC/aaf.png",
